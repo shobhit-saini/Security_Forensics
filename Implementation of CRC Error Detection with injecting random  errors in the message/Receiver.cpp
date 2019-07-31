c3db7558cc2fd3948835e42f7bf24578719ea75b
SHOBHIT <shobhitsaini95@gmail.com>
2019-07-31 13:03:33 +0530

    Update Receiver.cpp

--- Implementation of CRC Error Detection with injecting random  errors in the message/Receiver.cpp
+++ Implementation of CRC Error Detection with injecting random  errors in the message/Receiver.cpp
@@ -1,15 +1,46 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
 using namespace std;
 int main()
 {
-	string msg,crc,encoded="",res;
-	cout<<"Enter the msg";
-	getline(cin,msg);
+	string crc;
 	cout<<"Enter the CRC generator:";
 	getline(cin,crc);
-	int m=msg.length(),n=crc.length();
+    int n=crc.length();
+	   int net_socket;
+   net_socket=socket(AF_INET,SOCK_STREAM,0);
+   /*AF_INET:Family of IPV4
+     SOCK_STREAM:For TCP*/
+
+   //2.Specify an address for the socket
+   struct sockaddr_in server_address;
+   server_address.sin_family=AF_INET;
+   server_address.sin_port=htons(9007);//htons():Convert from Little to Big Endian
+   server_address.sin_addr.s_addr=INADDR_ANY;
+
+   int connection_status=connect(net_socket,(struct sockaddr*)&server_address,sizeof(server_address));
+
+  // check for error with the connection
+if(connection_status<0)
+{
+  printf("Error");
+}
+  //Recieve data from the server
+  char server_response[1024];
+  read(net_socket,server_response,1024);
+ string msg = server_response;
+ cout<<"received msg\t"<<msg<<endl;
+   cout<<"received msg len\t"<<msg.length()<<endl;
+//print out the server's reponse
+//printf("The server Sent the data:%s\n",server_response);
+
+
 	for(int i=0;i<=msg.length()-n;)
 	{
 		for(int j=0;j<n;j++)
@@ -18,10 +49,7 @@ int main()
 	 	}
 			for(;i<msg.length()&&msg[i]!='1';i++);
 	}
-		cout<<msg;
-		int res1=0;
-		stringstream res(msg);
-		res >> res1;
+	/*	int res1=stoi(msg);
 		if(res1==0)
 		{
 			cout<<"No Error";
@@ -29,5 +57,20 @@ int main()
 		else
 		{
 			cout<<"Error";
-		}
+		}*/
+	for(int i=0;i<msg.length();i++)
+	{
+	    if(msg[i]=='1')
+	    {
+	        cout<<"Error in msg";
+	        return 0;
+	    }
+	    
+	}	
+	cout<<"Msg received correctly";
+	
+		
+	//close the socket()
+    close(net_socket);
+return 0;
 }
