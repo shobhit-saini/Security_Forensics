57a23b8ef41e00a0d6d28c14e04757bfe2933490
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-15 01:28:31 +0530

    Create 1.tcl

---
+++ Tool_command_language/1.tcl
@@ -0,0 +1,10 @@
+#In tcl no need to declare the datatype it will take it as string
+set x 10
+set y "Shobhit"
+set z 30.34
+#$ symbol is used to fetch the value of variable
+puts $x
+puts $y
+puts $z
+set x $y
+puts $x
