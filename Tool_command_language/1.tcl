92efc9d7036b1f62d0f92b3d108d111eb64b6075
SHOBHIT <shobhitsaini95@gmail.com>
2019-08-15 01:30:24 +0530

    Update 1.tcl

--- Tool_command_language/1.tcl
+++ Tool_command_language/1.tcl
@@ -2,9 +2,14 @@
 set x 10
 set y "Shobhit"
 set z 30.34
+
 #$ symbol is used to fetch the value of variable
 puts $x
 puts $y
 puts $z
+
+#Assignment of variables
 set x $y
+
+#Print after assignment
 puts $x
